58ac6c9b57c591e0b1e114ff9be715cef0459bc1
Belisnalva Costa <bella-costa@hotmail.com.br>
2023-03-23 01:59:43 -0300

    Committing files

---
+++ temperatura.py
@@ -0,0 +1,13 @@
+#K = C + 273
+#K = 1.8 * C + 32
+#print("Kelvin: ", + "Fahrenheit: ")
+
+c = input('Digite a temperatura em Celsius:')
+c = eval(c)
+f = 1.8 * c + 32
+print(f, 'graus Fahrenhet.')
+
+print("*** Novo ***")
+f = input('Digite a temperatura em Celsius:')
+f = eval(c)
+celsius = 5/9(f - 32)
